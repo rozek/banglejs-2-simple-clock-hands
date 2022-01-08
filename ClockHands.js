9dbc229019b023743ec4bf7c8dffd9d858425ddb
Andreas Rozek <A.Rozek@gmx.de>
2022-01-08 13:58:06 +0100

    Update ClockHands.js

--- ClockHands.js
+++ ClockHands.js
@@ -63,7 +63,7 @@
     let HoursAngle   = (Hours+(Minutes/60))/12 * twoPi - Pi;
     let MinutesAngle = (Minutes/60)            * twoPi - Pi;
 
-    g.setColor(Settings.Foreground === 'Theme' ? Theme.fg : Settings.Foreground || '#000000');
+    g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
 
     transformPolygon(HourHandPolygon, CenterX,CenterY, HoursAngle);
     g.fillPoly(transformedPolygon);
@@ -72,7 +72,7 @@
     g.fillPoly(transformedPolygon);
 
     if (Seconds != null) {
-      g.setColor(Settings.Seconds === 'Theme' ? Theme.fgH : Settings.Seconds || '#FF0000');
+      g.setColor(Settings.Seconds === 'Theme' ? g.theme.fgH : Settings.Seconds || '#FF0000');
 
       let SecondsAngle = (Seconds/60) * twoPi - Pi;
 
